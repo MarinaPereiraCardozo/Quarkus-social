b27635b4a8b8b6ea74fd8157429484b35691031b
Marina <cardozo.marina.p@gmail.com>
2022-02-07 11:58:47 -0300

    Criação do endPoint para um usuário seguir outro

--- src/main/java/my/groupId/quarkussocial/domain/repository/FollowerRepository.java
+++ src/main/java/my/groupId/quarkussocial/domain/repository/FollowerRepository.java
@@ -1,10 +1,30 @@
 package my.groupId.quarkussocial.domain.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import my.groupId.quarkussocial.domain.model.Follower;
+import my.groupId.quarkussocial.domain.model.User;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
+
+    public boolean follows(User follower, User user){
+//        Map<String, Object> paramns = new HashMap<>();
+//        params.put("follower", follower);
+//        params.put("user", user);
+//        outra forma de fazer:
+        var params = Parameters.with("follower", follower)
+                .and("user", user)
+                .map();
+        PanacheQuery<Follower> query = find("follower = :follower and user = :user", params);
+        Optional<Follower> result = query.firstResultOptional();
+
+        return result.isPresent();
+    }
 }
--- src/main/java/my/groupId/quarkussocial/rest/FollowerResource.java
+++ src/main/java/my/groupId/quarkussocial/rest/FollowerResource.java
@@ -1,14 +1,16 @@
 package my.groupId.quarkussocial.rest;
 
+import my.groupId.quarkussocial.domain.model.Follower;
 import my.groupId.quarkussocial.domain.repository.FollowerRepository;
 import my.groupId.quarkussocial.domain.repository.UserRepository;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import my.groupId.quarkussocial.rest.dto.FollowerRequest;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/users/{idUser}/followers")
+@Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class FollowerResource {
@@ -16,9 +18,34 @@ public class FollowerResource {
     private FollowerRepository followerRepository;
     private UserRepository userRepository;
 
-    private FollowerResource(FollowerRepository followerRepository, UserRepository userRepository){
-
+    @Inject
+    public FollowerResource(
+            FollowerRepository followerRepository, UserRepository userRepository) {
         this.followerRepository = followerRepository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    @Transactional
+    public Response followUser(
+            @PathParam("userId") Long userId, FollowerRequest request){
+        var user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        var follower = userRepository.findById(request.getFollowerId());
+
+        boolean follows = followerRepository.follows(follower, user);
+
+        if(!follows){
+            var entity = new Follower();
+            entity.setUser(user);
+            entity.setFollower(follower);
+
+            followerRepository.persist(entity);
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
